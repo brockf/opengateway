2ac12f6007f1c9fecac804b93f90f9636ae84543
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2009-11-05 13:07:25 +0000

    Updated some code to best practices.   Added Response->Error() function.

--- opengateway/system/application/libraries/response.php
+++ opengateway/system/application/libraries/response.php
@@ -1,7 +1,8 @@
 <?php
+
 class Response
 {
-	function format_response($array = '')
+	function FormatResponse ($array = '')
 	{
 		//Load the CI object
 		$CI =& get_instance();
@@ -30,5 +31,23 @@ class Response
 			return FALSE;
 		}
 	}
+	
+	// return a formatted error response to the client
+	function Error ($code) {
+		if (!$code) {
+			die(show_error('No error code supplied.'));
+		}
+		
+		$errors = array(
+					'1000' => 'Invalid request.',
+					'1001' => 'Unable to authenticate.'
+				);
+				
+		$error_array = array(
+					'error' => $code,
+					'error_text' => $errors[$code]
+					);
+				
+		return $this->FormatResponse($error_array);
+	}
 }
-?>
