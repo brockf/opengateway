f8e3131d2f21785a14fd5579e8eb506c310f0263
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2009-12-17 06:07:28 +0000

    Created MY_Email to extend email class.

---
+++ opengateway/system/application/libraries/MY_email.php
@@ -0,0 +1,133 @@
+<?php
+
+class MY_Email extends CI_Email {
+
+    function My_Email()
+    {
+        parent::CI_Email();
+    }
+    
+    function TriggerTrip($trigger_type, $client_id, $charge_id = false, $subscription_id = false, $customer_id = false)
+    {
+    	$CI =& get_instance();
+    	$CI->load->model('email_model');
+    	
+    	// get trigger ID
+    	$trigger_type_id = $CI->email_model->GetTriggerId($trigger_type);
+    	
+    	if (!$trigger_type_id) {
+    		return FALSE;
+    	}
+    	
+    	// load all available data
+	    if ($subscription_id) {
+	    	$CI->load->model('subscription_model');
+	    	$subscription = $CI->subscription_model->GetRecurring($client_id, $subscription_id);
+	    }
+	    
+	    if ($charge_id) {
+	    	$CI->load->model('order_model');
+	    	$charge = $CI->order_model->GetCharge($client_id, $charge_id);
+	    }
+	    
+	    if ($customer_id) {
+	    	$CI->load->model('customer_model');
+	    	$customer = $CI->customer_model->GetCustomerDetails($client_id, $customer_id);
+	    }
+	    
+	    // dynamically get customer information for charge-related trips to save on SQL queries
+	    if (isset($subscription) and is_array($subscription['customer'])) {
+	    	$customer = $subscription['customer'];
+	    }
+	    elseif (isset($charge) and is_array($charge['customer'])) {
+	    	$customer = $charge['customer'];
+	    }
+	    
+	    // dynamically get plan-related info for recurring-related stuff
+	    if ($subscription_id) {
+	    	if (is_array($subscription['plan'])) {
+	    		$plan = $subscription['plan'];
+	    		$plan_id = $plan['id'];
+	    	}
+	    }
+	    
+	    if (!$plan_id) {
+	    	$plan_id = false;
+	    }
+	    
+	    // check to see if this triggers any emails for the client
+    	$emails = $CI->email_model->GetEmailsByTrigger($client_id, $trigger_type_id, $plan_id);
+    	
+    	if(!$emails) {
+    		return FALSE;
+    	}
+    	
+    	// build array of all possible variables, if they exist
+    	$variables = array();
+    	
+    	if (isset($charge) and is_array($charge)) {
+    		$variables['amount'] = $charge['amount'];
+    		$variables['date'] = date("Y-m-d h:i");
+    		$variables['charge_id'] = $charge['id'];
+    		$variables['card_last_four'] = $charge['card_last_four'];
+    	}
+    	
+    	if (isset($subscription) and is_array($subscription)) {
+    		$variables['recurring_id'] = $subscription['id'];
+    		$variables['start_date'] = $subscription['start_date'];
+    		$variables['end_date'] = $subscription['end_date'];
+    		$variables['expiry_date'] = $subscription['end_date'];
+    		$variables['next_charge_date'] = $subscription['next_charge_date'];
+    		
+    		if (isset($plan) and is_array($plan)) {
+    			$variables['plan_id'] = $plan['id'];
+    			$variables['plan_name'] = $plan['name'];
+    		}
+    	}
+    	
+    	if (isset($customer) and is_array($customer)) {
+    		$variables['customer_first_name'] = $customer['first_name'];
+    		$variables['customer_last_name'] = $customer['last_name'];
+    		$variables['customer_internal_id'] = $customer['internal_id'];
+    		$variables['customer_company'] = $customer['company'];
+    		$variables['customer_address_1'] = $customer['address_1'];
+    		$variables['customer_address_2'] = $customer['address_2'];
+    		$variables['customer_city'] = $customer['city'];
+    		$variables['customer_state'] = $customer['state'];
+    		$variables['customer_postal_code'] = $customer['postal_code'];
+    		$variables['customer_country'] = $customer['country'];
+    		$variables['customer_email'] = $customer['email'];
+    		$variables['customer_phone'] = $customer['phone'];
+    	}
+    	
+    	// load validation
+    	$CI->load->library('field_validation');
+    	
+		foreach ($emails as $email) {
+			// who is this going to?
+			$to_address = false;
+			if ($email['to_address'] == 'customer' and isset($customer['email']) and !empty($customer['email']) and $CI->field_validation->ValidateEmailAddress($customer['email'])) {
+				$to_address = $customer['email'];
+			}
+			elseif ($CI->field_validation->ValidateEmailAddress($email['to_address'])) {
+				$to_address = $email['to_address'];
+			}
+			
+			if ($to_address) {	
+				$subject = $email['email_subject'];
+				$body = $email['email_body'];
+				$from_name = $email['from_name'];
+				$from_email = $email['from_email'];
+				
+				// replace all possible variables
+				while (list($name,$value) = each($variables)) {
+					$subject = str_replace('[[' . $name . ']]',$value,$subject);
+					$body = str_replace('[[' . $body . ']]',$value,$body);
+				}
+				
+				// send the email
+				$this->load->library('email');
+			}		
+    	}
+    }
+}
