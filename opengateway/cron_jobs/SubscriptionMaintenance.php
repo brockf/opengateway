202f7c96c10e59a3db8daed9e45449356ccddfb1
daveryan <daveryan@3802b055-ba8f-4948-8871-0bf36bc10724>
2009-12-30 00:03:33 +0000

    Example of how to call the job from the command line.

---
+++ opengateway/cron_jobs/SubscriptionMaintenance.php
@@ -0,0 +1,5 @@
+<?php
+set_time_limit(0);
+$_SERVER['PATH_INFO'] = '/cron/SubscriptionMaintenance/';
+require_once '/../index.php';
+?>
---
+++ opengateway/cron_jobs/subMaintainance.cmd
@@ -0,0 +1,2 @@
+C:\wamp\bin\php\php5.3.0\php.exe SubscriptionMaintenance.php
+pause
