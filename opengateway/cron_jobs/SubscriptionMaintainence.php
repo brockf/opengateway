314acfb52f77bda341a717ba765eafe400d05b30
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-02-18 19:05:37 +0000

    Cron updates.

--- opengateway/cron_jobs/SubscriptionMaintainence.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-set_time_limit(0);
-$_SERVER['PATH_INFO'] = '/cron/subscription_maintenance/';
-require_once '/../index.php';
--- opengateway/cron_jobs/SubscriptionMaintenance.php
+++ opengateway/cron_jobs/SubscriptionMaintenance.php
@@ -1,5 +1,5 @@
 <?php
+
 set_time_limit(0);
-$_SERVER['PATH_INFO'] = '/cron/SubscriptionMaintenance/';
-require_once '/home/dave/workspace/openGW/opengateway/trunk/opengateway/index.php';
-?>
+$_SERVER['PATH_INFO'] = '/cron/subscription_maintenance/';
+require_once './../index.php';
