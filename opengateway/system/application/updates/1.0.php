876a15a12e27ac431a1fd1ee9b8a34cfd6744930
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-04-05 19:50:02 +0000

    Fixed 1.0 update.

--- opengateway/system/application/updates/1.0.php
+++ opengateway/system/application/updates/1.0.php
@@ -8,6 +8,8 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `version` (
 		  `db_version` float NOT NULL ,
 		  PRIMARY KEY  (`db_version`)
 		) ENGINE=MyISAM CHARSET=utf8 ;';
+		
+$sql[] = 'INSERT INTO `version` VALUES (\'1.0\');';
 
 foreach ($sql as $query) {
 	$CI->db->query($query);
