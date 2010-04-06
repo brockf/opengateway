b7567c8f449ae9a08ba8e527a448676ac58b1485
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-04-06 15:55:19 +0000

    Fixed End User/Service Provider names in database.  Version moved to 1.0.2.

---
+++ opengateway/system/application/updates/1.0.2.php
@@ -0,0 +1,13 @@
+<?php
+
+$CI =& get_instance();
+
+$sql = array();
+		
+$sql[] = 'UPDATE `client_types` SET `description`=\'End User\' WHERE `client_type_id`=\'2\'';
+
+$sql[] = 'UPDATE `client_types` SET `description`=\'Service Provider\' WHERE `client_type_id`=\'1\'';
+
+foreach ($sql as $query) {
+	$CI->db->query($query);
+}
