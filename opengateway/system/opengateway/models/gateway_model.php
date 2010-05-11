1f2e70a04bfd445ac68bdfed1259bc27417666ff
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-05-11 23:35:46 +0000

    Card number gets stripped of spaces, dashes, etc.

--- opengateway/system/opengateway/models/gateway_model.php
+++ opengateway/system/opengateway/models/gateway_model.php
@@ -150,6 +150,7 @@ class Gateway_model extends Model
 		$gateway_settings = $this->$gateway_name->Settings();
 		
 		// validate the Credit Card number
+		$credit_card['card_num'] = trim(str_replace(array(' ','-'),'',$credit_card['card_num']));
 		$credit_card['card_type'] = $this->field_validation->ValidateCreditCard($credit_card['card_num'], $gateway);
 		
 		if (!$credit_card['card_type']) {
