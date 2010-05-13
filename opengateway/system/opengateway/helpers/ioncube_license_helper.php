2b3f4c3a5b5984876884e173ba43edc11165c6dd
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-05-13 16:23:46 +0000

    Get IonCube license number.

--- opengateway/system/opengateway/helpers/ioncube_license_helper.php
+++ opengateway/system/opengateway/helpers/ioncube_license_helper.php
@@ -6,6 +6,8 @@ function ioncube_license () {
 	if (function_exists('ioncube_license_properties')) {
 		$license = ioncube_license_properties();
 		
+		die(print_r($license));
+		
 		if (isset($license['number'])) {
 			define("_LICENSENUMBER",$license['number']);
 		}
