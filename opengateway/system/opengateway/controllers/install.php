01c72934ec0a256c47d1954a0ef65fdc5e13f6e9
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-05-13 15:52:58 +0000

    Bugfixes.

--- opengateway/system/opengateway/controllers/install.php
+++ opengateway/system/opengateway/controllers/install.php
@@ -184,15 +184,18 @@ class Install extends Controller {
 	}
 	
 	function current_url() {
-		 $pageURL = 'http';
-		 if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-		 $pageURL .= "://";
-		 if ($_SERVER["SERVER_PORT"] != "80") {
-		  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-		 } else {
-		  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-		 }
-		 return $pageURL;
+		$pageURL = 'http';
+		if (isset($_SERVER["HTTPS"]) and $_SERVER["HTTPS"] == "on") {
+			$pageURL .= "s";
+		}
+		$pageURL .= "://";
+		if ($_SERVER["SERVER_PORT"] != "80") {
+			$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+		} else {
+			$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+		}
+		
+		return $pageURL;
 	}
 	
 	function admin () {
