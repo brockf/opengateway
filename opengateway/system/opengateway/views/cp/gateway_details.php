3db43016b392fff39d06408a7ed15e0e397836e5
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-05-21 06:04:56 +0000

    Put "test mode" warning in there.

--- opengateway/system/opengateway/views/cp/gateway_details.php
+++ opengateway/system/opengateway/views/cp/gateway_details.php
@@ -1,5 +1,8 @@
 <?=$this->load->view(branded_view('cp/header'));?>
 <h1><?=$form_title;?></h1>
+<p class="warning"><span>Is your gateway in test mode?  Even when "Test mode" is specified below, your transactions can
+still be processed as live, real transactions if your gateway is not in test mode.  You must set your gateway
+to test mode in your gateway control panel.  "Test mode" below only indicates which of your gateway's servers to use.</span></p>
 <form class="form" id="form_plan" method="post" action="<?=$form_action;?>">
 <input type="hidden" name="external_api" value="<?=$external_api;?>" />
 <fieldset>
