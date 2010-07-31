602e42264354c8bc37a07ae2dfc5021ebde82bd1
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-07-31 20:57:23 +0000

    Removed PayLeap.

--- opengateway/system/opengateway/updates/1.4.83.php
+++ opengateway/system/opengateway/updates/1.4.83.php
@@ -5,7 +5,7 @@ $CI =& get_instance();
 $sql = array();
 
 // add ChangeRecurringPlan payment method
-$sql[] = 'INSERT INTO `external_apis` (`name`, `display_name`, `prod_url`, `test_url`, `dev_url`, `arb_prod_url`, `arb_test_url`, `arb_dev_url`) VALUES (\'payleap\', \'PayLeap\', \'\', \'\', \'\', \'\', \'\', \'\');';
+//$sql[] = 'INSERT INTO `external_apis` (`name`, `display_name`, `prod_url`, `test_url`, `dev_url`, `arb_prod_url`, `arb_test_url`, `arb_dev_url`) VALUES (\'payleap\', \'PayLeap\', \'\', \'\', \'\', \'\', \'\', \'\');';
 
 foreach ($sql as $query) {
 	$CI->db->query($query);
