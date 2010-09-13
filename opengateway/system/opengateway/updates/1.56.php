6bffc770353650f49b600d92a3123eacc7751e67
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2010-09-13 19:53:29 +0000

    v1.5.6 includes the "renew" parameter.

--- opengateway/system/opengateway/updates/1.56.php
+++ opengateway/system/opengateway/updates/1.56.php
@@ -5,7 +5,7 @@ $CI =& get_instance();
 $sql = array();
 
 // add `renewed` field to `subscriptions`
-$sql[] = 'ALTER TABLE `subscriptions` ADD COLUMN `renewed` tinyint(1) AFTER `active`';
+$sql[] = 'ALTER TABLE `subscriptions` ADD COLUMN `renewed` INT(11) AFTER `active`';
 
 foreach ($sql as $query) {
 	$CI->db->query($query);
