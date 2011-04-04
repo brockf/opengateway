04594f29f0e266cfbde1c7d5ab5eae9cf238a878
brockf <brockf@3802b055-ba8f-4948-8871-0bf36bc10724>
2011-04-04 22:06:02 +0000

    Don't charge initial charge if the amount is $0 (adjusted by a coupon).

--- opengateway/system/opengateway/models/gateway_model.php
+++ opengateway/system/opengateway/models/gateway_model.php
@@ -646,7 +646,7 @@ class Gateway_model extends Model
 		// get subscription
 		$subscription = $CI->recurring_model->GetRecurring($client_id, $subscription_id);
 		// is there a charge for today?
-		$charge_today = (date('Y-m-d', strtotime($subscription['date_created'])) == date('Y-m-d', strtotime($subscription['start_date']))) ? TRUE : FALSE;
+		$charge_today = ((float)$amount != 0 and date('Y-m-d', strtotime($subscription['date_created'])) == date('Y-m-d', strtotime($subscription['start_date']))) ? TRUE : FALSE;
 		
 		// set last_charge as today, if today was a charge
 		if ($charge_today === TRUE) {
